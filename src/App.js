846c5443444914ea76c034bae0c162dfc8118e6b
IshantL <ishantlambhate@gmail.com>
2018-06-19 03:54:19 +0000

    Project Initiation

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+import './App.css';
+import Navigation from './components/Navigation/Navigation';
+import Logo from './components/Logo/Logo';
+import 'tachyons'
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+       <Navigation/>
+       <Logo/>
+       {/*
+       <ImageLinkForm/>
+       <FaceRecognition/>*/}
+      </div>
+    );
+  }
+}
+
+export default App;
