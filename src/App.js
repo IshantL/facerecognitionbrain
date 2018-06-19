861f15db26d336facd149710716afb691255d5fb
IshantL <ishantlambhate@gmail.com>
2018-06-19 06:30:55 +0000

    Adding the components and basic layout of our app

--- src/App.js
+++ src/App.js
@@ -2,16 +2,35 @@ import React, { Component } from 'react';
 import './App.css';
 import Navigation from './components/Navigation/Navigation';
 import Logo from './components/Logo/Logo';
+import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
+import Rank from './components/Rank/Rank'
+import Particles from 'react-particles-js';
+
 import 'tachyons'
 
+const particlesOptions={
+  particles: {
+    number:{
+      value:30,
+      density:{
+        enable:true,
+        value_area:300
+      }
+    }
+  }
+}
 class App extends Component {
   render() {
     return (
       <div className="App">
+      <Particles className='particles'
+        params={particlesOptions}
+      />
        <Navigation/>
        <Logo/>
-       {/*
+       <Rank/>
        <ImageLinkForm/>
+       {/*
        <FaceRecognition/>*/}
       </div>
     );
---
+++ src/components/Rank/Rank.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Rank=()=>{
+return(
+	<div>
+	<div className='white f3'>Ishant, your current Rank is</div>
+	<div className='white f1'>5</div>
+	</div>
+	);
+}
+
+export default Rank;
